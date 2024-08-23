ccffc56513605fb47c188eed2f570ed997f1c488
mathealgou <mathealgou@gmail.com>
2024-08-23 18:47:50 -0300

    feat: Now in a window

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import time
 from system_metrics import (get_cpu_usage, get_memory_usage,
                             get_gpu_usage, setup_get_gpu_usage_async, stop_get_gpu_usage)
-from screen import draw
+from screen import draw, Data, setup_screen
 import asyncio
 import signal
+import tkinter as tk
 
 # Things needed to deal with threads and signals
 stop_flag = False
@@ -21,12 +22,11 @@ signal.signal(signal.SIGINT, signal_handler)
 
 async def main():
     await setup_get_gpu_usage_async()
+    root = setup_screen()
     try:
         while not stop_flag:
-            cpu_usage = get_cpu_usage()
-            memory_usage = get_memory_usage()
-            gpu_usage = get_gpu_usage()
-            draw(cpu_usage, memory_usage, gpu_usage)
+            data = Data(get_cpu_usage(), get_memory_usage(), get_gpu_usage())
+            draw(data, root)
             time.sleep(.1)
     finally:
         stop_get_gpu_usage()
--- screen.py
+++ screen.py
@@ -1,20 +1,57 @@
 import os
+import tkinter as tk
+import signal
 
 # add description to functions
 
 
-def draw(cpu_usage: float, memory_usage: float, gpu_usage: float):
+class Data():
+    def __init__(self, cpu_usage: float, memory_usage: float, gpu_usage: float):
+        self.cpu_usage = cpu_usage
+        self.memory_usage = memory_usage
+        self.gpu_usage = gpu_usage
+
+
+cpu_text = None
+
+memory_text = None
+
+gpu_text = None
+
+
+def setup_screen():
+    root = tk.Tk()
+    root.title("System Metrics")
+    root.geometry("300x200")
+    root.resizable(False, False)
+    global cpu_text, memory_text, gpu_text
+    cpu_text = tk.StringVar()
+    memory_text = tk.StringVar()
+    gpu_text = tk.StringVar()
+
+    cpu_label = tk.Label(root, textvariable=cpu_text)
+    cpu_label.pack()
+    memory_label = tk.Label(root, textvariable=memory_text)
+    memory_label.pack()
+    gpu_label = tk.Label(root, textvariable=gpu_text)
+    gpu_label.pack()
+    # make the window appear on top of all other windows
+    root.attributes("-topmost", True)
+
+    # emmit a signal when the window is closed, so the program can stop
+    # (this is listened in the main.py file)
+    root.protocol("WM_DELETE_WINDOW",
+                  lambda: signal.raise_signal(signal.SIGINT))
+
+    return root
+
+
+def draw(data: Data, root: tk.Tk):
     """
         Draw the system metrics on the screen
     """
-    clear()
-    print(f"CPU Usage: {cpu_usage:.2f}%")
-    print(f"Memory Usage: {memory_usage:.2f}%")
-    print(f"GPU Usage: {gpu_usage:.2f}%")
-
-
-def clear():
-    """
-        Clear the screen
-    """
-    os.system('cls' if os.name == 'nt' else 'clear')
+    cpu_text.set(f"CPU: {data.cpu_usage:.2f}%")
+    memory_text.set(f"Memory: {data.memory_usage:.2f}%")
+    gpu_text.set(f"GPU: {data.gpu_usage:.2f}%")
+    root.update()
+    root.update_idletasks()
